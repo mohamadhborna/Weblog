f6ad30ddfe007e998f184bd6dace079f3bece89d
mohamadborna <bornaamir8@gmail.com>
2021-03-30 08:14:15 +0330

    add configuration parts

--- app.js
+++ app.js
@@ -1,5 +1,10 @@
 const express = require("express");
+const dotEnv = require("dotenv");
+
+//* Load Config
+dotEnv.config({path :"./config/config.env"})
 
 const app = express();
+const port = process.env.PORT || 3000;
 
-app.listen(4200 , () =>{console.log("Server is running on port 4200")});
+app.listen(port , () =>{console.log(`Server is running on port ${port} in ${process.env.NODE_ENV} mode`)});
